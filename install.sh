d833e8b9904a716beb426612c9b70daa86f5ca2a
Matheus Fidelis <matheus.scarpato@superlogica.com>
2016-08-27 23:49:34 -0300

    Validação do Root

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 echo "INSTALLING PPA DEBIAN UTILITY"
-cp add-apt-repository.sh /usr/sbin/add-apt-repository
-chmod +x /usr/sbin/add-apt-repository
 
-echo "Now you can install Ubuntu PPA Repositories on your Debian System"
-echo "# sudo add-apt-repository ppa:ppaname"
-echo "thanks :)"
-echo "https://github.com/msfidelis" 
+if [[ $EUID -ne 0 ]]; then
+	echo -e "Este Script deve ser executado como root \n"
+	exit 1
+else
 
+	cp add-apt-repository.sh /usr/sbin/add-apt-repository
+	chmod +x /usr/sbin/add-apt-repository
+
+	echo "Now you can install Ubuntu PPA Repositories on your Debian System"
+	echo "# sudo add-apt-repository ppa:ppaname"
+	echo "thanks :)"
+	echo "https://github.com/msfidelis" 
+fi
