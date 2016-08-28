d15dc755b523a5515929a64ff109f63f9f4ac99b
msfidelis <matheus.scarpato@superlogica.com>
2016-08-27 23:41:32 -0300

    First Commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "INSTALLING PPA DEBIAN UTILITY"
+cp add-apt-repository.sh /usr/sbin/add-apt-repository
+chmod +x /usr/sbin/add-apt-repository
+
+echo "Now you can install Ubuntu PPA Repositories on your Debian System"
+echo "# sudo add-apt-repository ppa:ppaname"
+echo "thanks :)"
+echo "https://github.com/msfidelis" 
+
